11d976ee5f9de6d61f03d6068ffa8b8ae789972b
Muhammad Shazaib Khan <124022640+Muhammad-Shazaib-Khan@users.noreply.github.com>
2023-11-29 15:23:33 +0500

    Add files via upload

---
+++ assets/JS/gsap.js
@@ -0,0 +1,191 @@
+gsap.from(".logo h1 , .nav__menu ul li",{
+    y:-100,
+    duration:0.8,
+    delay: 1,
+    opacity: 0,
+    stagger:0.1
+})
+gsap.from(".home-img",{
+    x:-100,
+    duration:0.8,
+    delay: 0.3,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".home-img",
+        scroller:"body"
+    }
+
+});
+gsap.from(".home-text h2",{
+    y:-100,
+    duration:0.8,
+    delay: 0.5,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".home-text h2",
+        scroller:"body"
+    }
+});
+gsap.from(".home-text p",{
+    x:100,
+    duration:0.8,
+    delay: 0.5,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".home-text p",
+        scroller:"body"
+    }
+});
+gsap.from(".home-text .h-b",{
+    y:100,
+    duration:0.8,
+    delay: 0.5,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".home-text .h-b",
+        scroller:"body"
+    }
+});
+gsap.from(".aerobic h1",{
+    y:-100,
+    duration:0.8,
+    delay: 1,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".aerobic h1",
+        scroller:"body"
+    }
+
+});
+
+gsap.from(".aer-text , aer-text ul li",{
+    x:-100,
+    duration:0.8,
+    delay: 1,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".aer-text , aer-text ul li",
+        scroller:"body"
+    }
+});
+gsap.from(".aerobic .sb-img",{
+    x:100,
+    duration:0.8,
+    delay: 1,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".aerobic .sb-img",
+        scroller:"body"
+    }
+});
+gsap.from(".aero-btn",{
+    y:100,
+    duration:0.8,
+    delay: 0.3,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".aero-btn",
+        scroller:"body"
+    }
+});
+gsap.from(".poss .tital-headding",{
+    y:100,
+    duration:0.8,
+    delay: 0.3,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".poss .tital-headding",
+        scroller:"body"
+    }
+});
+gsap.from(".poss .pic-contect",{
+    y:100,
+    duration:0.8,
+    delay: 0.3,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".poss .pic-contect",
+        scroller:"body"
+    }
+});
+gsap.from(".poss .pic-seccontect",{
+    y:-100,
+    duration:0.8,
+    delay: 0.3,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".poss .pic-seccontect",
+        scroller:"body"
+    }
+});
+gsap.from(".wsm .tital-headding, .Introduction h2",{
+    y:-100,
+    duration:0.8,
+    delay: 0.3,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".wsm .tital-headding, .Introduction h2",
+        scroller:"body"
+    }
+});
+gsap.from(".wsm .sb-img",{
+    x:100,
+    duration:0.8,
+    delay: 0.3,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".wsm .sb-img",
+        scroller:"body"
+    }
+});
+gsap.from(".wsm ul li",{
+    x:-100,
+    duration:0.8,
+    delay: 0.3,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".wsm ul li",
+        scroller:"body"
+    }
+});
+gsap.from(".wsm .smtaital",{
+    y:100,
+    duration:0.8,
+    delay: 0.3,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".wsm .smtaital",
+        scroller:"body"
+    }
+});
+gsap.from(".footer ,.copy-rights ",{
+    duration:1,
+    delay: 0.3,
+    opacity: 0,
+    scrollTrigger:{
+        trigger:".footer , .copy-rights",
+        scroller:"body"
+    }
+});
+gsap.from(".poses-text .p-hedding , .poses-text img , .p-text h1, .poses-text p",{
+    scale: 0,
+    duration:1,
+    delay: 0.3,
+    opacity: 0,
+    stagger:0.5,
+    scrollTrigger:{
+        trigger:".poses-text .p-hedding , .poses-text img , .poses-text .p-text h1 , .poses-text p",
+        scroller:"body"
+    }
+})
+gsap.from(".how , .how .headding , .slider-container, .artist",{
+    y:-100,
+    duration:1,
+    delay: 0.3,
+    opacity: 0,
+    stagger:0.5,
+    scrollTrigger:{
+        trigger:".how , .how .headding , .slider-container, .artist",
+        scroller:"body"
+    }
+})
